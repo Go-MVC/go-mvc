f64df3e8721a36f505f2e2bfaeac2ccdd4a4bc65
Joshua <jdong42@gmail.com>
2016-11-02 08:46:37 -0500

    wip

--- app.go
+++ app.go
@@ -1,12 +1,25 @@
 package main
 
+// For more info check out:
+// https://www.iron.io/an-easier-way-to-create-tiny-golang-docker-images/
+
 import (
     "fmt"
     "os"
     "github.com/urfave/cli"
-//    "github.com/docker/docker/api/types"
-//    "github.com/docker/docker/client"
+    "github.com/docker/docker/api/types"
+    "github.com/docker/docker/client"
 )
+
+func buildDockerImage() {
+    // https://godoc.org/github.com/docker/docker/client#Client.ContainerExecStart
+    //cli.ContainerCreate()
+    //cli.ContainerExecStart()
+}
+
+func buildBinary() {
+    //docker run --rm -it -v "$GOPATH":/gopath -v "$(pwd)":/app -e "GOPATH=/gopath" -w /app golang:1.4.2 sh -c 'CGO_ENABLED=0 go build -a --installsuffix cgo --ldflags="-s" -o hello'
+}
 
 func main() {
     app := cli.NewApp()
@@ -27,6 +40,7 @@ func main() {
             Usage:   "build a docker image",
             Action:  func(c *cli.Context) error {
                 fmt.Println("image built.")
+                buildDockerImage()
                 return nil
             },
         },
@@ -35,7 +49,8 @@ func main() {
             Aliases: []string{"u"},
             Usage:   "upload image to cloud",
             Action:  func(c *cli.Context) error {
-                fmt.Println("...")
+                fmt.Println("uploading image...")
+                buildBinary()
                 return nil
             },
         },
